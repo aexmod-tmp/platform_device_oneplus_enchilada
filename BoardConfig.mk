9787f1165734b10fafb70d31cac1624cc8ee925b
Martinusbe <martinusbe@gmail.com>
2017-07-07 09:32:02 +0200

    lockscreen charging info
    real time values
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,9 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CONFIG := enchilada_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Platform
 # TARGET_BOARD_PLATFORM := sdm845
 BOARD_USES_QCOM_HARDWARE := true
