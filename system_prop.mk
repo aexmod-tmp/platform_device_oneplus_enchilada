22a6173f4c650b5ed5722a8ba86b325565cf6ff7
Marko Man <darkobas@gmail.com>
2018-08-27 20:21:33 +0200

    prop: disable inline rotator
    Change-Id: I9c650976dc27d5da72ea309eb7f02c9772aef42a
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -30,7 +30,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.gfx_ubwc_disable=0 \
-    debug.sf.latch_unsignaled=1
+    debug.sf.latch_unsignaled=1 \
+    sdm.debug.disable_inline_rotator=1 \
+    sdm.debug.disable_inline_rotator_secure=1
 
 # IOP
 PRODUCT_PROPERTY_OVERRIDES += \
