7f73f462fbdde8756f94e7d28f426f1a54d2c63b
Vachounet <vachounet@live.fr>
2018-10-09 16:16:24 +0200

    set client package name
    * Introduce a new boot receiver that force camera client package name via HIDL HAL to fix portrait mode
    
    Change-Id: Ibac00612b794eefd3c6d7ba6fa4e44fcc0197cc1
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+# Camera
+PRODUCT_PACKAGES_DEBUG += \
+    CameraPackage
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
