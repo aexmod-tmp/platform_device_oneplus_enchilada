13390b513a78fa97fb6050798c3eaf006d6f9dc3
Marko Man <darkobas@gmail.com>
2018-12-19 01:40:07 +0100

    prop: enable cne
    Change-Id: Icef53693647aad2a4119d8dfe2f2c73bf57d62ce
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -27,6 +27,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.camera.preview.size=1920x1080 \
     vendor.camera.aux.packagelist="com.oneplus.camera"
 
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cne.feature=1
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.gfx_ubwc_disable=0 \
