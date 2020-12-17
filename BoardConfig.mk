raghavt20/PA-unify-device_xiaomi_violet

violet: media: Add and publish Codec2 HAL service
*Add 2 codec2 relates libs to PRODUCT_PACKAGES in order to copy
them to /vendor/lib/

Change-Id: I56da6b774cf0f9b9e628886d7bfe22f5ccae4258

BoardConfig.mk
@@ -146,6 +146,7 @@ TARGET_USES_QTI_MAPPER_EXTENSIONS_1_1 := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true

aexmod-tmp/platform_device_oneplus_enchilada

Signed-off-by: DennySPb <dennyspb@gmail.com>

BoardConfig.mk
@@ -156,6 +156,7 @@ TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(VENDOR_PATH)/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 
 # Partitions